9a879b765ce8847af81d3a638414af30b1b32cec
halqupati <hudurah737@gmail.com>
2024-03-12 01:55:14 +0300

    order importing packges

--- lib/features/todo/pages/homepage.dart
+++ lib/features/todo/pages/homepage.dart
@@ -3,23 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_todo2/common/helpers/notification_helper.dart';
-import 'package:riverpod_todo2/common/utils/constants.dart';
-import 'package:riverpod_todo2/common/widgets/expansion_tile.dart';
-import 'package:riverpod_todo2/common/widgets/reusable_text.dart';
-import 'package:riverpod_todo2/features/todo/controllers/todo/todo_provider.dart';
-import 'package:riverpod_todo2/features/todo/controllers/xpantion_provider.dart';
-import 'package:riverpod_todo2/features/todo/widgets/tomorrow_list.dart';
 
-import '../../../common/models/task_model.dart';
+import '../../../common/helpers/notification_helper.dart';
+import '../../../common/utils/constants.dart';
 import '../../../common/widgets/appstyle.dart';
 import '../../../common/widgets/custom_text.dart';
 import '../../../common/widgets/hieght_spacer.dart';
+import '../../../common/widgets/reusable_text.dart';
 import '../../../common/widgets/width_spacer.dart';
+
 import '../widgets/completed_task.dart';
 import '../widgets/dayAfterTomorrow.dart';
 import '../widgets/today_task.dart';
-import '../widgets/todo_title.dart';
+import '../controllers/todo/todo_provider.dart';
+import '../widgets/tomorrow_list.dart';
 import 'add_todo.dart';
 
 class HomePage extends ConsumerStatefulWidget {
